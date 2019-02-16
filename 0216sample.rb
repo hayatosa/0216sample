a2522a6f6e41ce1bcdbc1160dc957551edd459be
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2019-02-16 20:56:18 +0900

    add_TEXT

--- 0216sample.rb
+++ 0216sample.rb
@@ -1,29 +1,3 @@
-hp = 0
-
-if hp > 10
-  puts "勇者は元気だ"
-elsif hp > 5
-  puts "勇者は少し弱っている"
-elsif hp > 0
-  puts "勇者は死にそうだ"
-else
-  puts "勇者は力尽きた"
-end
-
-month = 4
-case month
-when 3..5
-  puts "春"
-when 12..2
-  puts "冬"
-else
-  puts "いい季節"
-end
-
-#三項演算子
-hp = 10
-puts hp < 5 ? "勇者のHPは5より低い":"勇者のHPは5より高い"
-
 pokemon_hp = 100
 pokemon_attack = 20
 pokemon_defense = 50
@@ -35,9 +9,13 @@ satoshi_defense = 80
 satoshi_damage = pokemon_attack - satoshi_defense
 satoshi_hp = satoshi_hp - satoshi_damage
 
-puts "------------------------------------"
-puts "敵に#{satoshi_damage}のダメージを与えた"
-puts "残りhpは#{satoshi_hp}だ"
+puts <<~TEXT
+------------------------------------
+敵に#{satoshi_damage}のダメージを与えた
+
+残りhpは#{satoshi_hp}だ
+
+TEXT
 
 if satoshi_hp > 60
   puts "敵は元気だ"
@@ -54,8 +32,12 @@ end
 pokemon_damage = satoshi_attack - pokemon_defense
 pokemon_hp = pokemon_hp - pokemon_damage
 
-puts "pokemonは#{pokemon_damage}のダメージを受けた"
-puts "pokemonのhpは#{pokemon_hp}だ"
+puts <<~TEXT
+pokemonは#{pokemon_damage}のダメージを受けた
+
+pokemonのhpは#{pokemon_hp}だ
+
+TEXT
 
 if pokemon_hp > 60
   puts "pokemonは元気だ"
